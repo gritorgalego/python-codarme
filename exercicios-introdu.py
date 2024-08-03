a52bd3a2b17ae55f7bbf5e221fda611bd29aefa6
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-08-03 17:34:33 -0300

    new file:   aula_02.py 	new file:   aula_03.py 	new file:   exercicios-introdu.py

---
+++ exercicios-introdu.py
@@ -0,0 +1,21 @@
+a = "Vitor Galego"
+b = 1606
+c = 16.06
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# - - - - - - - - - - - - - - - - - - 
+
+valor_compras = 26.50
+desconto = valor_compras * 0.1
+quantidade_itens = 2
+valor_final = valor_compras - desconto
+custo_medio = valor_final / quantidade_itens
+
+print("O valor final foi de R$ ", valor_final)
+
+print("O custo médio dos itens foi de R$ ", custo_medio)
