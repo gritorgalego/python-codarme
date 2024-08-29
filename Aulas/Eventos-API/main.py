842f32dcad33c45368cc3183dec6f22e11b249dc
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-08-29 17:42:05 -0300

    error handler

--- Aulas/Eventos-API/main.py
+++ Aulas/Eventos-API/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 from evento import Evento
 from evento_online import EventoOnline
 import json
@@ -21,3 +21,15 @@ def listar_eventos():
     for ev in eventos:
         eventos_dict.append(ev.__dict__)
     return jsonify(eventos_dict)
+
+@app.errorhandler(404)
+def nao_encontrado(erro):
+    return (jsonify(erro=str(erro)), 404)
+    
+@app.route("/api/eventos/<int:id>/")
+def detalhar_evento(id):
+    for ev in eventos:
+        if ev.id == id:
+            return jsonify(ev.__dict__)
+        
+    abort(404, "Evento não econtrado")
