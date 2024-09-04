65dd81924752dda21c91acb030a152553d853226
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-09-04 14:57:32 -0300

    Criando novo evento/Error Handler 400

--- Aulas/Eventos-API/main.py
+++ Aulas/Eventos-API/main.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from evento import Evento
 from evento_online import EventoOnline
 import json
 
 app = Flask(__name__)
-
 
 ev_online = EventoOnline("Live de Python")
 ev2_online = EventoOnline("Live de JavaScript")
@@ -21,6 +20,33 @@ def listar_eventos():
     for ev in eventos:
         eventos_dict.append(ev.__dict__)
     return jsonify(eventos_dict)
+
+@app.route("/api/eventos/", methods=["POST"])
+def criar_evento():
+    # Parsing
+    data = json.loads(request.data)
+    nome = data.get("nome")
+    local = data.get("local")
+
+    # Validação
+    if not nome:
+        abort(400,"'nome' precisa ser informado!" )
+        
+    # Criando evento
+    if local:
+        evento = Evento(nome=nome, local=local)
+    else:
+        evento = EventoOnline(nome=nome)
+    eventos.append(evento)
+    
+    return {
+        "id": evento.id,
+        "url": f"/api/eventos/{evento.id}/" 
+    }
+
+@app.errorhandler(400)
+def nao_encontrado(erro):
+    return (jsonify(erro=str(erro)), 400)
 
 @app.errorhandler(404)
 def nao_encontrado(erro):
