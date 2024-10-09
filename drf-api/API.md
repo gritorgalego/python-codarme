cd6437bf569f5dfcbeb5e1b13b1cb3bfcf193b72
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-10-08 21:28:01 -0300

    Criando views, urls, modelos, serializer e configurações gerais. Listando "TODOs"

---
+++ drf-api/API.md
@@ -0,0 +1,8 @@
+# API
+
+- Listar horários: GET /horarios/
+- Listar agendamentos: GET /agendamentos/
+- Detalhar agendamento: GET /agendamentos/<id>/ 
+- Criar agendamento: POST /agendamentos/
+- Excluir agendamento: DELETE /agendamentos/<id>/
+- Editar um agendamento: PUT/PATCH /agendamentos/<id>/
--- drf-api/agenda/views.py
+++ drf-api/agenda/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
 
-# Create your views here.
+
+def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
+    serializer = AgendamentoSerializer(obj)
+    return JsonResponse(serializer.data)
