a1e8ff0dcd8881ddcfe53493ef28dbb6b6333e39
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-10-10 15:45:48 -0300

    Adiciona view para listar e exibir agendamento

--- drf-api/agenda/views.py
+++ drf-api/agenda/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
+from rest_framework.decorators import api_view
 
 
+@api_view(http_method_names=["GET"])
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.data)
+
+
+@api_view(http_method_names=["GET"])
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+    return JsonResponse(serializer.data, safe=False)
