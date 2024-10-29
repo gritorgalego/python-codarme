08af66ee22b23b4c2a1a4631276576aae85440c0
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-10-29 19:56:08 -0300

    Cancelando agendamentos

--- drf-api/API.md
+++ drf-api/API.md
@@ -4,6 +4,5 @@
 - Listar agendamentos: GET /agendamentos/ - Feito
 - Detalhar agendamento: GET /agendamentos/<id>/ - Feito
 - Criar agendamento: POST /agendamentos/ - Feito
-- Excluir agendamento: DELETE /agendamentos/<id>/ -
-- Editar um agendamento: PUT / agendamentos/<id>/ - Feito
-- Editar uma agendamento parcialmente: PATCH / agendamentos/<id>/ -
+- Excluir agendamento: DELETE /agendamentos/<id>/ - Feito
+- Editar uma agendamento parcialmente: PATCH / agendamentos/<id>/ - Feito
--- drf-api/agenda/views.py
+++ drf-api/agenda/views.py
@@ -3,9 +3,10 @@ from django.http import JsonResponse
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-@api_view(http_method_names=["GET", "PATCH"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
     if request.method == "GET":
         obj = get_object_or_404(Agendamento, id=id)
@@ -25,12 +26,16 @@ def agendamento_detail(request, id):
             obj.save()
             return JsonResponse(serializer.validated_data, status=200)
         return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        obj = get_object_or_404(Agendamento, id=id)
+        obj.cancelado = True
+        return Response(status=204)
 
 
 @api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
     if request.method == "GET":
-        qs = Agendamento.objects.all()
+        qs = Agendamento.objects.filter(cancelado=False)
         serializer = AgendamentoSerializer(qs, many=True)
         return JsonResponse(serializer.data, safe=False)
     if request.method == "POST":
