e7e76951a3fa6fd9a5adaa1772b36b4945e1e3d2
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-11-06 14:46:26 -0300

    Refatoração

--- drf-api/agenda/views.py
+++ drf-api/agenda/views.py
@@ -8,19 +8,17 @@ from rest_framework.response import Response
 
 @api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
     if request.method == "GET":
-        obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(obj)
         return JsonResponse(serializer.data)
     if request.method == "PATCH":
-        obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.validated_data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "DELETE":
-        obj = get_object_or_404(Agendamento, id=id)
         obj.cancelado = True
         return Response(status=204)
 
