225824aa02eedf90fecebf4f5980e0bd7faad36e
Vitor Avila Galego <vitorgalego1606@gmail.com>
2024-11-06 14:39:20 -0300

    Removendo arquivos indesejados do controle de versão

--- drf-api/agenda/views.py
+++ drf-api/agenda/views.py
@@ -14,16 +14,9 @@ def agendamento_detail(request, id):
         return JsonResponse(serializer.data)
     if request.method == "PATCH":
         obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(data=request.data, partial=True)
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            obj.data_horario = validated_data.get("data_horario", obj.data_horario)
-            obj.nome_cliente = validated_data.get("nome_cliente", obj.nome_cliente)
-            obj.email_cliente = validated_data.get("email_cliente", obj.email_cliente)
-            obj.telefone_cliente = validated_data.get(
-                "telefone_cliente", obj.telefone_cliente
-            )
-            obj.save()
+            serializer.save()
             return JsonResponse(serializer.validated_data, status=200)
         return JsonResponse(serializer.errors, status=400)
     if request.method == "DELETE":
@@ -42,12 +35,6 @@ def agendamento_list(request):
         data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.objects.create(
-                data_horario=validated_data["data_horario"],
-                nome_cliente=validated_data["nome_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=validated_data["telefone_cliente"],
-            )
+            serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
